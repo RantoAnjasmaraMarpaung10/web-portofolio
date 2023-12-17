1dd8fb1eface3c0bbabb243ee37faf88b96c96c1
L3viathan10 <milyhyaranto@gmail.com>
2023-12-18 00:11:56 +0700

    update2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
         <a className={styles.title} href="/">
-            Portofolio
+            Ranto
         </a>
         <div className={styles.menu}>
             <img className={styles.menuBtn} 
